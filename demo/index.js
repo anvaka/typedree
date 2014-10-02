928e21bd900afd79dc0c21606804e6fce6ba1f33
anvaka <anvaka@gmail.com>
2014-10-02 00:35:07 -0700

    Just getting basic api going

---
+++ demo/index.js
@@ -0,0 +1,28 @@
+var createQuadTree = require('../');
+
+var bounds = {
+  x: 0,
+  y: 0,
+  width: 100,
+  height: 100
+};
+
+var tree = createQuadTree(bounds);
+var bodies = createBodies(2);
+
+bodies.forEach(function (body) {
+  tree.insert(body);
+});
+
+bodies.forEach(function (body, i) {
+  tree.updateForce(body);
+  console.log(i, body.fx, body.fy);
+});
+
+function createBodies(n) {
+  var bodies = [];
+  for (var i = 0; i < n; ++i) {
+    bodies.push({ x: Math.random() * 100, y: Math.random() * 100, mass: Math.random * 78 });
+  }
+  return bodies;
+}
